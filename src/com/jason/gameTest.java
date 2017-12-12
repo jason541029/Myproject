8b3f4b861d5f9bb13339fc4d7ed4d8d38d7d8ad6
Jason541029 <jtr5494@gmail.com>
2017-12-12 16:13:14 +0800

    跑跑

--- src/com/jason/gameTest.java
+++ src/com/jason/gameTest.java
@@ -8,36 +8,79 @@ public class gameTest {
 		// TODO Auto-generated method stub
 		
 		Scanner scanner = new Scanner(System.in);
-		int d = -1;
+		int pos = 0;
+		int row = 3;
+		int col = 5;
+		int hp = 100;
+		int d = 3;
 		while(d !=0 ){
 			System.out.print("Please enter a number 8,2,4,6分別為上下左右:");
 		String dir = scanner.nextLine();
 
 	d = Integer.parseInt(dir);
-	
-	
 
-		
-		
 	switch(d){
-case 2:
+	case 2:
 	System.out.println("向下");
+		if(pos/col < row-1){
+		pos=pos+col;
+		hp=hp-5;
+		System.out.println("pos:"+pos);
+		System.out.println("HP:"+hp);
+		
+		
+	}else{
+		hp=hp-30;
+		System.out.println("HP:"+hp);
+	}
 	break;
 	
-case 8:
-	System.out.println("向上");
+	case 8:
+		System.out.println("向上");
+		if(pos/col>row+1){
+			pos= pos-col;
+		hp=hp-5;
+		System.out.println("pos:"+pos);
+		System.out.println("HP:"+hp);
+		
+	}else{
+		hp=hp-30;
+		System.out.println("HP:"+hp);
+	}
 	break;
-case 4:
+	case 4:
 	System.out.println("向左");
-	break;
-case 6:
+	if(pos % col != 0){
+		pos=pos-1;
+		hp=hp-5;
+		System.out.println("pos:"+pos);
+		System.out.println("HP:"+hp);
+		
+		
+	}else{
+		hp=hp-30;
+		System.out.println("HP:"+hp);
+	}
+		break;
+	case 6:
 	System.out.println("向右");
-	break;
+	if(pos%col !=4){
+		pos=pos+1;
+		hp=hp-5;
+		System.out.println("pos:"+pos);
+			System.out.println("HP:"+hp);
+		
+	}else{
+		hp=hp-30;
+			System.out.println("HP:"+hp);
+	}
+		break;
+	
 
 case 0:
 	System.out.println(" 結束 ");
 	break;
-}
-}
-}
+	}
+		}
+	}
 	}
--- src/com/jason/oo/Student.java
+++ src/com/jason/oo/Student.java
@@ -6,15 +6,18 @@ public class Student {
 	int math;
 	int chinese;
 	String name;
-	public Student(String n, int e, int m, int c){
-		name = n;
-		english = e;
-		math = m;
-		chinese = c;
+	public Student(String nane, int english, int math, int chiness){
+		this.name = name;
+		this.english = english;
+		this.math = math;
+		this.chinese = chiness;
 	}
-	
+	public Student(String n){
+		this.name = name;
+	}
 	public void print(){
-		System.out.println(english+"\t"+math+"\t"+chinese);
+		System.out.println(english+"\t"	
+				+math+"\t"+chinese);
 	}
 	public int getAverage(){
 		return (english+math+chinese)/3;
