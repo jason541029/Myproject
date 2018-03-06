27650c967b48ff4d1cb2fc93e8bfefa4ca3b5871
Jason541029 <jtr5494@gmail.com>
2018-03-06 16:19:52 +0800

    string

---
+++ src/com/java2/object/test1.java
@@ -0,0 +1,15 @@
+package com.java2.object;
+
+public class test1 {
+
+	
+
+int height;
+String name;
+
+public test1(String name , int height)	{
+	this.name = name;
+	this.height = height;
+	}
+
+}
---
+++ src/com/java2/object/test2.java
@@ -0,0 +1,10 @@
+package com.java2.object;
+
+public class test2 extends test1 {
+	private int weight;
+	private int bmi;
+	public test2(String name , int height)	{
+super(name, height);
+	}
+
+}
