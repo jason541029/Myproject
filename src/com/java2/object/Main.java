c849de228fb2ad57fa5f1c4ee2abfccbccc690da
Jason51029 <jtr5494@gmail.com>
2018-05-08 16:24:21 +0800

    hash

---
+++ src/com/java2/object/Main.java
@@ -0,0 +1,95 @@
+package com.java2.object;
+/*import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+
+		List<Integer> list = Arrays.asList(2,0,1,7);
+		
+
+		    //write your code here
+		System.out.println(list);
+
+		  }
+}*/
+	
+//}
+//import java.util.*;
+
+//public class Main {
+
+ // public static void main(String[] args) {
+
+  // List<Integer> list = Arrays.asList(3,4,5);
+
+    //write your code here
+
+
+   // System.out.println(list);
+
+ // }
+//}
+/*import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+
+    List<String> nameList = new ArrayList<>(Arrays.asList("Mr.Green", "Mr.Yellow", "Mr.Red"));
+for(String name : nameList){
+System.out.println(name);
+    //write your code here
+
+
+  }
+}
+}*/
+
+/*import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+
+    //write your code here
+
+
+    SortedSet<String> set = new TreeSet(Arrays.asList("Alpha", "Gamma", "Omega"));
+    System.out.println(set);
+
+  }
+}*/
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+
+    Set<String> nameSet = new TreeSet<>(Arrays.asList("Mr.Green", "Mr.Yellow", "Mr.Red"));
+
+    //write your code here
+for(String name : nameSet){
+System.out.println(name);
+
+
+  }
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
