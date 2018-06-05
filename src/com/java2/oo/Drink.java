a827bc169796ba4228962c6d5bb62585062e65de
Jason541029 <jtr5494@gmail.com>
2018-06-05 23:27:48 +0800

    machine

--- src/com/java2/oo/Drink.java
+++ src/com/java2/oo/Drink.java
@@ -4,13 +4,16 @@ public class Drink {
 int id ;
 String name;
 int price;
+public Drink() {
+
+}
 public Drink(int id, String name, int price) {
 	super();
 	this.id = id;
 	this.name = name;
 	this.price = price;
 }
-public int getId() {
+public int getId() {//右鍵get set 
 	return id;
 }
 public void setId(int id) {
--- src/com/java2/oo/VendingMain.java
+++ src/com/java2/oo/VendingMain.java
@@ -1,7 +1,9 @@
 package com.java2.oo;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class VendingMain {
 
@@ -13,7 +15,18 @@ try {
 	fr = new FileReader("Vending.txt");
 	BufferedReader in = new BufferedReader(fr);
 	String line = in.readLine();
-	String[] tokens = line.split(",")
+	String[] tokens = line.split(",");
+	int count = Integer.parseInt(tokens[0]);
+	for (int i = 0; i < count; i++){
+		String name = tokens[i*2+1];
+		int price = Integer.parseInt(tokens[i*2+2]);
+		vm.drinks.add(new Drink(i+1, name, price));
+	}
+	vm.on();
+} catch (FileNotFoundException e) {
+	e.printStackTrace();
+} catch (IOException e) {
+	e.printStackTrace();
 }
 	}
 
