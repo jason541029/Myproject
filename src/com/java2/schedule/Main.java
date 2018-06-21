535e2d67dbc71e703ca7b8fa916bdeae4ba4bb25
Jason541029 <jtr5494@gmail.com>
2018-06-21 19:14:37 +0800

    reader

--- src/com/java2/schedule/Main.java
+++ src/com/java2/schedule/Main.java
@@ -1,14 +1,31 @@
 package com.java2.schedule;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-
+import java.util.List;
 public class Main {
-	
-	public static void main(String[] args) {
-	
 		List<Course>courses = new ArrayList<>();
 		public Main() {
+			readCourses();
+			
+			
+		}
+		private void readCourses() {
+			// TODO Auto-generated method stub
+			try {
+				BufferedReader in  = new BufferedReader(new FileReader(("schedule.txt")));
+				
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+			
+		}
 	}
+
 	
 
-} 
