c791a363b2d2e76f7cef8be43fe15e5996a82653
Jason541029 <jtr5494@gmail.com>
2018-06-25 01:00:10 +0800

    bingo

--- src/com/java2/oo/Bingo.java
+++ src/com/java2/oo/Bingo.java
@@ -1,8 +1,14 @@
 package com.java2.oo;
 
+import java.util.ArrayList;
+
 public class Bingo {
 	public static void main(String[] args) {
 		
-		
+	int[][]card = new int[5][5];
+	ArrayList<Integer> play = new ArrayList<Integer>();
+			boolean valid =  false;
+	int tmp = 0;
+	
 	}
 }
---
+++ src/com/javachat/oo/Bingo.java
@@ -0,0 +1,10 @@
+package com.javachat.oo;
+
+public class Bingo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
